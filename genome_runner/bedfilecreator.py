8e1b95cfd363b3a5f44eef690b2c6071c1e37654
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2012-08-07 19:54:09 -0500

    fixed exon bug

--- genome_runner/bedfilecreator.py
+++ genome_runner/bedfilecreator.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from logging import FileHandler,StreamHandler
 import os
 import ftplib
 import sqlite3
@@ -11,13 +12,21 @@ import gzip
 import re
 import collections
 import copy
+import traceback  as trace
 # connection information for the uscs ftp server
 server = 'hgdownload.cse.ucsc.edu'
 directory = '/goldenPath/{}/database'
 username = 'anonymous'
 password = ''
-eventlog = logging.getLogger("grdbcreator")
-logfilename = "gr_log.txt"
+
+logger = logging.getLogger('genomerunner.dbcreator')
+hdlr = logging.FileHandler('genomerunner_dbcreator.log')
+hdlr_std = StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.addHandler(hdlr_std)
+logger.setLevel(logging.INFO)
 			
 # downloads the specified file from uscs.  Saves it with a .temp extension untill the download is complete.
 def download_uscs_file(organism,filename,downloaddir):
@@ -25,46 +34,46 @@ def download_uscs_file(organism,filename,downloaddir):
 	in a folder with the same name as the organism.
 	'''
 	outputpath = ''
-	if downloadtodir != None and downloadtodir != '':
+	if downloaddir != None and downloaddir != '':
 		outputdir = os.path.join(downloaddir,organism)
 	else:
 		outputdir = organism
 	try:
 		if os.path.exists(outputdir) == False and outputdir != '':
-			print "creating directory {}".format(outputdir)
+			logger.info( "creating directory {}".format(outputdir))
 			os.makedirs(outputdir)
 	except Exception, e:
-		print e
-		eventlog.warning("ERROR: could not create folder at {} for {}".format(outputdir,filename),logfilename)
+		logger.warning( e)
+		logger.warning("ERROR: could not create folder at {} for {}".format(outputdir,filename))
 		return '' 
 	
 	try:
 		outputpath = os.path.join(outputdir,filename)
 		if not os.path.exists(outputpath):
 			with open(outputpath + ".temp",'wb') as fhandle:  
-				print 'Downloading {} from USCS'.format(filename)
+				logger.info( 'Downloading {} from USCS'.format(filename))
 				ftp = ftplib.FTP(server)
 				ftp.login(username,password)
 				ftp.cwd(directory.format(organism))
 				ftp.retrbinary('RETR ' + "{}".format(filename),fhandle.write)
 				os.rename(outputpath+".temp",outputpath)
-				print 'Finished downloading {} from USCS'.format(filename)
+				logger.info( 'Finished downloading {} from USCS'.format(filename))
 		else:
-			print '{} already exists, skipping download'.format(outputpath)
+			logger.info( '{} already exists, skipping download'.format(outputpath))
 	except Exception, e:
-		print e
-		eventlog.warning("ERROR: could not download the {} sql file. Names ARE case sensitive.".format(filename),logfilename)
+		logger.warning( e)
+		logger.warning("ERROR: could not download the {} sql file. Names ARE case sensitive.".format(filename))
 		return '' 
 
 	return outputpath 
 
 
-def download_trackdb(organism):
+def download_trackdb(organism,outputdir):
 	''' Downloads the trackdb.sql and trackDb.txt.gz from the USCS ftp server and saves it in a folder with the same name as the organism.
 		Returns the path of the downloaded .sql file
 	'''
-	sqloutputpath = download_uscs_file(organism,"trackDb.sql",downloadtodir)
-	dataoutpath = download_uscs_file(organism,"trackDb.txt.gz",downloadtodir)
+	sqloutputpath = download_uscs_file(organism,"trackDb.sql",outputdir)
+	dataoutpath = download_uscs_file(organism,"trackDb.txt.gz",outputdir)
 	' replace all of the \\\n characters in the html column with <br />'
 	text = gzip.open(dataoutpath).read()
 	with gzip.open(dataoutpath,'wb') as sw:
@@ -76,18 +85,18 @@ def download_trackdb(organism):
 
 def extract_bed6(outputpath,datapath,colnames):
 	colstoextract = ['chrom','chromStart','chromEnd','name','score','strand']
-	print "Outpath is: {}".format(outputpath)
+	logger.info( "Outpath is: {}".format(outputpath))
 	with gzip.open(datapath) as dr:
 		with gzip.open(outputpath,"wb") as bed:
 			while True:
-				line = dr.readline().strip()
-				if line == "":
-					break
-				r  = dict(zip(colnames,line.split('\t')))
-				row = []
-				for col in colnames:
-					row.append(r[col]) 
-				bed.write("\t".join(map(str,row))+"\n")
+					line = dr.readline().strip('\r').rstrip('\n')
+					if line == "":
+						break
+					r  = dict(zip(colnames,line.split('\t')))
+					row = []
+					for col in colstoextract:
+						row.append(r[col]) 
+					bed.write("\t".join(map(str,row))+"\n")
 
 
 def extract_bed3(outputpath,datapath,colnames):
@@ -95,11 +104,33 @@ def extract_bed3(outputpath,datapath,colnames):
 	with gzip.open(outputpath,"wb") as bed:
 		with gzip.open(datapath) as dr:
 			while True:
-				line = dr.readline().strip()
+				line = dr.readline().strip('\r').rstrip('\n')
 				if line == "":
 					break
 				r  = dict(zip(colnames,line.split('\t')))
 				row = [r["chrom"],r["chromStart"],r["chromEnd"],".",".","."]
+				bed.write("\t".join(map(str,row))+"\n")
+
+def extract_bed5(outputpath,datapath,colnames):
+	with gzip.open(outputpath,"wb") as bed:
+		with gzip.open(datapath) as dr:
+			while True:
+				line = dr.readline().rstrip('\r').rstrip('\n')
+				if line == "":
+					break
+				r  = dict(zip(colnames,line.split('\t')))
+				row = [r["chrom"],r["chromStart"],r["chromEnd"],r["name"],r["score"],"."]
+				bed.write("\t".join(map(str,row))+"\n")
+
+def extract_bed4(outputpath,datapath,colnames):
+	with gzip.open(outputpath,"wb") as bed:
+		with gzip.open(datapath) as dr:
+			while True:
+				line = dr.readline().rstrip('\r').rstrip('\n')
+				if line == "":
+					break
+				r  = dict(zip(colnames,line.split('\t')))
+				row = [r["chrom"],r["chromStart"],r["chromEnd"],r["name"],".","."]
 				bed.write("\t".join(map(str,row))+"\n")
 
 def extract_genepred(outputpath,datapath,colnames):
@@ -110,7 +141,7 @@ def extract_genepred(outputpath,datapath,colnames):
 			from path import basename
 			with gzip.open(exonpath+".temp","wb") as exonbed:
 				while True:
-					line = dr.readline().strip()
+					line = dr.readline().rstrip('\r').rstrip('\n')
 					if line == "":
 						break
 					r = dict(zip(colnames,line.split('\t')))
@@ -118,7 +149,7 @@ def extract_genepred(outputpath,datapath,colnames):
 					row = [r['chrom'],r['txStart'],r['txEnd'],r['name'],'.',r['strand']]
 					bed.write("\t".join(map(str,row))+"\n")
 					# extract the exon data
-					for (s,e) in zip(r["exonStarts"].split(","),r["exonStarts"].split(",")):
+					for (s,e) in zip(r["exonStarts"].split(","),r["exonEnds"].split(",")):
 						if s != '':
 							rowexon = [r['chrom'],s,e,r['name'],'.',r['strand']]
 							exonbed.write("\t".join(map(str,rowexon))+"\n")
@@ -134,25 +165,55 @@ def get_column_names(sqlfilepath):
 	# creates a tuple containing the column names
 	tbdcolumns = re.findall("\n\s+`(.+?)`", tdbsql, re.DOTALL)
 	return tbdcolumns	
-preparebed = {"bed 6" : extract_bed6, "bed 6 +" : extract_bed6, "bed 3" : extract_bed3,"genePred" : extract_genepred}
-numdownloaded = {"bed 6" : 0, "bed 6 +" : 0, "bed 3" : 0, "genePred" : 0}
+preparebed = {"bed 6" : extract_bed6, 
+				"bed 6 +" : extract_bed6,
+				"bed 12 +": extract_bed6,
+				"bed 12 .": extract_bed6,
+				"bed 12": extract_bed6,
+				"bed 10": extract_bed6,
+				"bed 9 +": extract_bed6,
+				"bed 9 .": extract_bed6,
+				"bed 9": extract_bed6,
+				"bed 8 +": extract_bed6,
+				"bed 8 .": extract_bed6,
+				"bed 6 .": extract_bed6,
+				"bed 5 +": extract_bed5,
+				"bed 5 .": extract_bed5,
+				"bed 5": extract_bed5,
+				"bed 4 +": extract_bed4,
+				"bed 4 .": extract_bed4,
+				"bed 4": extract_bed4,
+				"bed 3 +": extract_bed3,
+				"bed 3 .": extract_bed3,
+				"bed 3" : extract_bed3,
+				"genePred xenoRefPep xenoRefMrna": extract_genepred,
+				"genePred vegaPep": extract_genepred,
+				"genePred sgpPep": extract_genepred,
+				"genePred refPep refMrna": extract_genepred,
+				"genePred nscanPep": extract_genepred,
+				"genePred knownGenePep knownGeneMrna": extract_genepred,
+				"genePred genscanPep": extract_genepred,
+				"genePred geneidPep": extract_genepred,
+				"genePred ensPep": extract_genepred,
+				"genePred acemblyPep acemblyMrn": extract_genepred,
+				"genePred" : extract_genepred}
+numdownloaded = {"bed 6" : 0, "bed 6 +" : 0, "bed 3" : 0, "genePred" : 0,"bed 9 +": 0,"bed 12 +": 0, "bed 12 .": 0, "bed 12": 0, "bed 10": 0, "bed 9 +": 0, "bed 9 .": 0, "bed 9": 0, "bed 8 +": 0, "bed 8 .": 0, "bed 6 .": 0, "bed 5 +": 0, "bed 5 .": 0, "bed 5": 0, "bed 4 +": 0, "bed 4 .": 0, "bed 4": 0, "bed 3 +": 0, "bed 3 .": 0, "genePred xenoRefPep xenoRefMrna": 0, "genePred vegaPep": 0, "genePred sgpPep": 0, "genePred refPep refMrna": 0, "genePred nscanPep": 0, "genePred knownGenePep knownGeneMrna": 0, "genePred genscanPep": 0, "genePred geneidPep": 0, "genePred ensPep": 0, "genePred acemblyPep acemblyMrn": 0}
 
 
 def download_bedfiles(trackdbpath,organism):
 	outputdir = os.path.dirname(trackdbpath)
 	trackdb = load_tabledata_dumpfiles(os.path.splitext(trackdbpath)[0])
 	prog, num = 0,len(trackdb) 
+	notsuptypes = set([])
 	for row in trackdb:	
-		print 'Processing files {} of {}'.format(prog,num)
+		logger.info( 'Processing files {} of {}'.format(prog,num))
 		if row['type'] in preparebed:
 			# DEBUG this line limits the number of GRF to download
-			for d in numdownloaded.values():
-				print d
 			if numdownloaded[row["type"]] <=5000000:
 				sqlpath = download_uscs_file(organism,row["tableName"] + ".sql","downloads")
 				download_uscs_file(organism,row["tableName"] + ".txt.gz","downloads")
 				if sqlpath != '':
-					print "converting",row['tableName'], " into proper bed format"
+					logger.info( "converting"+row['tableName']+ " into proper bed format")
 					try:
 						outpath = os.path.join(outputdir,row["grp"],'Tier' + row["visibility"],row["tableName"]+".gz")
 						if not os.path.exists(os.path.dirname(outpath)):
@@ -162,18 +223,26 @@ def download_bedfiles(trackdbpath,organism):
 							if os.path.exists(outpath+".temp"):
 								os.remove(outpath+".temp")
 							# converts the uscs data into propery bed format
-							print "Converting into proper bed format. {}".format(os.path.splitext(sqlpath)[0] + ".txt.gz")
+							logger.info( "Converting into proper bed format. {}".format(os.path.splitext(sqlpath)[0] + ".txt.gz"))
 							preparebed[row["type"]](outpath+".temp",os.path.splitext(sqlpath)[0]+".txt.gz",get_column_names(os.path.splitext(sqlpath)[0]+".sql"))
 							# remove the .temp file extension to activate the GF
 							os.rename(outpath+".temp",outpath)
 						else:
-							print "{} already exists, skipping extraction".format(outpath)
+							logger.info( "{} already exists, skipping extraction".format(outpath))
 						numdownloaded[row["type"]] += 1
 					except Exception, e:
-						print "ERROR unable to convert {} into bed".format(row["tableName"])
-						print e
+						trace.print_exc(file=sys.stdout)
+						logger.warning( "ERROR unable to convert {} into bed".format(row["tableName"]))
+						logger.warning( e)
 						continue
+		else:
+			if 'big' not in row['type']:
+				notsuptypes.add(row['type'])
+
 		prog += 1
+	logger.info( "The following types are not supported (includes all 'big' file types): " + str(notsuptypes))
+	for k,d in numdownloaded.iteritems():
+		logger.info( k + ":" + str(d))
 	return "created database"
 	
 
@@ -201,16 +270,16 @@ if __name__ == "__main__":
 	parser.add_argument('--organism','-g', help='The USCS code of the organism to be downloaded (example: hg19 (human))')
 	parser.add_argument('--filename','-f', help='The name of the specific file to download (example: trackDb.sql). If provided, only this file will be downloaded')
 	args = vars(parser.parse_args())
-	downloadtodir='download'
+	outputdir='released'
 	if args['output'] is not None:
-		downloadtodir = args['output']
+		outputdir = args['output']
 	if args['organism'] is not None and args['filename'] is not None:
-		download_uscs_file(args['organism'],args['filename'],downloadtodir)
+		download_uscs_file(args['organism'],args['filename'],outputdir)
 	elif args['organism'] is not None and args['filename'] is None:
-		trackdbpath = download_trackdb(args['organism'])
+		trackdbpath = download_trackdb(args['organism'],outputdir)
 		download_bedfiles(trackdbpath,args['organism'])
 	else:
-		trackdbpath = download_trackdb('hg19')
-		print trackdbpath
+		trackdbpath = download_trackdb('hg19',outputdir)
+		logger.info( trackdbpath)
 		download_bedfiles(trackdbpath,'hg19')
 
