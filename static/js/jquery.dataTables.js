413a6befb8f691b4c93b59056410dc9f765d5a94
Lukas Cara <lks_cara@yahoo.com>
2013-08-19 17:05:35 -0500

    Fixed #35

--- static/js/jquery.dataTables.js
+++ static/js/jquery.dataTables.js
@@ -11861,3 +11861,17 @@
 	 *  @param {object} o DataTables settings object {@link DataTable.models.oSettings}
 	 */
 }(jQuery, window, document, undefined));
+
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+    "scientific-pre": function ( a ) {
+        return parseFloat(a);
+    },
+ 
+    "scientific-asc": function ( a, b ) {
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+ 
+    "scientific-desc": function ( a, b ) {
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+} );
