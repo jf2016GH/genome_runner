060e62716eb19eb9b7ff5ae89614f4f3b439487e
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2013-08-24 13:07:46 -0500

    db_readme copied into package folder, setup.py update

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ import setuptools
 import os
 
 setup(
-    name='GenomeRunnerSNP',
+    name='GenomeRunner SNP',
     version='0.1.0',
-    author='Mikhail Dozmorov',
-    author_email='mikhail-dozmorov@omrf.org',
+    author='Mikhail G. Dozmorov, Lukas R. Cara, Cory B. Giles',
+    author_email='mikhail.dozmorov@gmail.com, lks_cara@yahoo.com, mail@corygil.es',
+    data_files=[('grsnp',['grsnp_db_readme.txt',]),],
     packages=['grsnp'],
     package_dir={"grsnp": "grsnp"},
     scripts=[os.path.join(r,f) for r,d,fs in os.walk("grsnp/bin") for f in fs if f.endswith(".py") or f.endswith(".sh") or "bedToBigBed" in f],
-    url='',
+    url='http://www.genomerunner.org',
     license='LICENSE.txt',
-    description='',
+    description='GenomeRunner SNP: Interpreting genome veriation within epigenomic context',
     long_description=open('README').read())
