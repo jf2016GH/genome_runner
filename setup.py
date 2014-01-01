cad86a2d6b62ee2f6e179e1135728196db7afb1c
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2014-01-01 12:33:37 -0600

    Setup.py fully functional, Readme.rst updated to reflect these changes

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools.command.install import install as _install
 import os
 import subprocess
 
-
-
 def scrip_installer(command_subclass):
     """A decorator for classes subclassing one of the setuptools commands.
 
@@ -16,7 +14,24 @@ def scrip_installer(command_subclass):
 
     def modified_run(self):
         # installs all prerequisites and GRTK
-        grtk_install = """mkdir downloads\ncd downloads\nsudo apt-get -y install python-setuptools python-pip python-dev parallel r-base-core bedtools samtools tabix kyotocabinet-utils realpath python-cherrypy3 python-numpy python-scipy\nsudo apt-get -y upgrade gcc\nsudo pip install -U cython\nwget -N https://github.com/bedops/bedops/releases/download/v2.3.0/bedops_linux_x86_64-v2.3.0.tar.bz2\nsudo tar xjvf bedops_linux_x86_64-v2.3.0.tar.bz2 -C /usr/local/\nsudo wget -np -R -A "bedToBigBed" -A "bedGraphToBigWig" -A "bigWig*" -A "bigBed*" -N -e robots=off -r -P /usr/local/bin -nd "http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/"\nsudo wget -o /usr/local/bin/rowsToCols http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/rowsToCols\nsudo chmod a+x /usr/local/bin/*\ngit clone https://mdozmorov@bitbucket.org/wrenlab/grtk.git\ncd grtk\nsudo python setup.py install\ncd ../..\nsudo rm -r downloads"""
+        grtk_install = """
+            mkdir downloads
+            cd downloads
+            sudo apt-get -y install parallel r-base-core bedtools samtools tabix kyotocabinet-utils realpath
+            sudo apt-get -y install python-setuptools python-pip python-dev python-cherrypy3 python-numpy python-scipy python-rpy2 python-simplejson python-mako
+            sudo apt-get -y upgrade gcc
+            sudo pip install -U cython
+            wget -N https://github.com/bedops/bedops/releases/download/v2.3.0/bedops_linux_x86_64-v2.3.0.tar.bz2
+            sudo tar xjvf bedops_linux_x86_64-v2.3.0.tar.bz2 -C /usr/local/
+            sudo wget -np -R -A "bedToBigBed" -A "bedGraphToBigWig" -A "bigWig*" -A "bigBed*" -N -e robots=off -r -P /usr/local/bin -nd "http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/"
+            sudo wget -o /usr/local/bin/rowsToCols http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/rowsToCols
+            sudo chmod a+x /usr/local/bin/*
+            git clone https://mdozmorov@bitbucket.org/wrenlab/grtk.git
+            cd grtk
+            sudo python setup.py install
+            cd ../..
+            sudo rm -r downloads
+        """
         subprocess.Popen(grtk_install,stdout=subprocess.PIPE,shell=True).wait()
         # Install the R packages required by grsnp
         r_packages_install = "sudo Rscript installer.R"
@@ -40,7 +55,7 @@ setup(
     version='0.1.0',
     author='Mikhail G. Dozmorov, Lukas R. Cara, Cory B. Giles',
     author_email='mikhail.dozmorov@gmail.com, lks_cara@yahoo.com, mail@corygil.es',
-    data_files=[('grsnp', ['grsnp_db_readme.txt']),],
+    #data_files=[('grsnp', ['grsnp_db_readme.txt']),],
     packages=['grsnp'],
     package_dir={"grsnp": "grsnp"},
     #include_package_data= True,
@@ -305,19 +320,20 @@ setup(
     "frontend/templates/help.mako",
     "frontend/templates/overview.mako",
     "frontend/__init__.py",
+    "grsnp_db_readme.txt"
     ],
     },
     scripts=[os.path.join(r,f) for r,d,fs in os.walk("grsnp/bin") for f in fs if f.endswith(".py") or f.endswith(".sh") or "bedToBigBed" in f],
     url='http://www.genomerunner.org',
     license='LICENSE.txt',
-    install_requires=[
-    "cython",
-    "pybedtools",
-    "bx-python",
-    "rpy2",
-    "mako",
-    "simplejson",
-    ],
+#    install_requires=[
+#    "cython",
+#    "pybedtools",
+#    "bx-python",
+#    "rpy2",
+#    "mako",
+#    "simplejson",
+#    ],
     description='GenomeRunner Web: Functional interpretation of SNPs within epigenomic context',
     long_description=open('README.rst').read(),
     cmdclass={
