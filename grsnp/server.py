eab95669ab67558c8c7a9fae24f2b42e86967cb2
Lukas Cara <lks_cara@yahoo.com>
2014-01-03 12:17:07 -0600

    heatmaps work

--- grsnp/server.py
+++ grsnp/server.py
@@ -473,7 +473,7 @@ class WebUI(object):
 
 	@cherrypy.expose
 	def get_cluster(self,run_id):
-		mat_path = os.path.join(os.path.join(results_dir,run_id,"clustered.json"))
+		mat_path = os.path.join(results_dir,run_id,"clustered.json")
 		if os.path.exists(mat_path):
 			with open(mat_path) as f:
 				return f.read()
@@ -481,7 +481,7 @@ class WebUI(object):
 	
 	@cherrypy.expose
 	def get_pcc(self,run_id):
-		mat_path = os.path.join(os.path.join(results_dir,run_id,"pcc_matrix.json"))
+		mat_path = os.path.join(results_dir,run_id,"pcc_matrix.json")
 		if os.path.exists(mat_path):
 			with open(mat_path) as f:
 				return f.read()
