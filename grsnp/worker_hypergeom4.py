f133eaefd90c9bb60dc442f16107907b7ec62bc3
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2014-07-11 14:55:11 -0500

    Fixed #53 #54. Annotation analysis checkbox working

--- grsnp/worker_hypergeom4.py
+++ grsnp/worker_hypergeom4.py
@@ -8,5 +8,5 @@ app = Celery('grsnp')
 app.config_from_object('grsnp.celeryconfiguration')
 
 @app.task(ignore_result=False)
-def run_hypergeom(fois, gfs, bg_path,outdir,job_name="",zip_run_files=False,bkg_overlaps_path="",gr_data_dir = "" ,run_annotation=True,run_randomization_test=False,padjust="None",pct_score="",organism=""):	
+def run_hypergeom(fois, gfs, bg_path,outdir,job_name="",zip_run_files=False,bkg_overlaps_path="",gr_data_dir = "" ,run_annotation=False,run_randomization_test=False,padjust="None",pct_score="",organism=""):	
 	grsnp.hypergeom4.run_hypergeom(fois, gfs, bg_path,outdir,job_name,zip_run_files,bkg_overlaps_path,gr_data_dir,run_annotation,run_randomization_test,padjust,pct_score,organism)
