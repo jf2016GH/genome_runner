337331a07061993073cc5bd21aa88f5b3d6fb365
mdozmorov <lks_cara@yahoo.com>
2016-06-17 17:16:35 -0400

    Cleaned up Celery config file

--- grsnp/celeryconfiguration.py
+++ grsnp/celeryconfiguration.py
@@ -1,11 +1,5 @@
 from kombu import Exchange, Queue
 
-db_num = "0"
-
-# redis server config code
-# redis_port = 7775
-# BROKER_URL = "redis://localhost:{}/".format(redis_port) + db_num
-6
 
 # Workers should run as an unprivileged user.
 CELERYD_USER="ubuntu"
@@ -23,22 +17,3 @@ CELERY_IMPORTS = ("grsnp")
 CELERYD_PREFETCH_MULTIPLIER = 1 
 CELERY_TASK_PUBLISH_RETRY = False
 
-
-#CELERY_SEND_TASK_SENT_EVENT = True
-
-
-## Celery queue settings
-#CELERY_ROUTES = ({'grsnp.worker_hypergeom4.run_hypergeom': {'queue': 'short_runs'}},
-#					{'grsnp.worker_hypergeom4.run_hypergeom': {'queue': 'long_runs'}}
-#				)
-
-#default_exchange = Exchange('default', type='direct')
-
-#CELERY_QUEUES = (
-#    Queue('default', default_exchange, routing_key='default'),
-#    Queue('long_runs', default_exchange, routing_key='grsnp'),
-#    Queue('short_runs', default_exchange, routing_key='grsnp')
-#)
-#CELERY_DEFAULT_QUEUE = 'default'
-#CELERY_DEFAULT_EXCHANGE = 'default'
-#CELERY_DEFAULT_ROUTING_KEY = 'default'
